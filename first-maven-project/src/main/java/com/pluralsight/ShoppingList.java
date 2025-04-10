4472faad7e345175772f817015da2ecd03c696a9
Shiraaz Ahmed <shiraaza08@gmail.com>
2025-04-10 11:19:48 -0400

    completed ShoppingList app

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+// your code goes here
+     System.out.println("Egg, Chicken, Bread, Milk, Butter, Waffles, Apples, Orange, Banana, Biscuit");
+    }
+}
