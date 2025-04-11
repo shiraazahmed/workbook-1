8578af67da9f8237335854e12b5019b9921ae869
Shiraaz Ahmed <shiraaza08@gmail.com>
2025-04-11 14:04:43 -0400

    completed Mod3 Exercise 2 - MathApp

---
+++ first-maven-project/src/main/java/com/pluralsight/Declare.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+import javax.swing.plaf.basic.BasicBorders;
+
+public class Declare {
+    public static void main(String[] args) {
+        long identificationNum = 786000;
+        String vehicleMake = "Honda Accord";
+        String vehicleColor = "Grey";
+        boolean towingPackage = true;
+        String odometerReading = "125000";
+        long price = 10000;
+        String qualityRating = "A, B, C";
+        String phoneNumber = "123-346-6789";
+        String sSN = "123-33-4444";
+        long zipCode = 28134;
+
+        System.out.println("Sure!"+ identificationNum);
+        System.out.println("Sure!"+ vehicleMake);
+        System.out.println("Sure!"+ odometerReading);
+        System.out.println("Sure!"+ towingPackage);
+        System.out.println("Sure!"+ price);
+        System.out.println("Sure!"+ phoneNumber);
+        System.out.println("Sure!"+ sSN);
+        System.out.println("Sure!"+ zipCode);
+        System.out.println("Also I got my car towed last week!");
+    }
+}
---
+++ first-maven-project/src/main/java/com/pluralsight/MathApp/Java.java
@@ -0,0 +1,57 @@
+package com.pluralsight.MathApp;
+//    Q1
+public class Java {
+    public static void main(String[] args) {
+      int bobSalary = 10000;
+      int garySalary = 11000;
+      int highestSalary = garySalary;
+      System.out.println("The Highest Salary is " + garySalary);
+
+//Q2
+    int carPrice = 10000;
+    int truckPrice = 15000;
+    int smallestPrice = Math.min(carPrice, truckPrice);
+    System.out.println("The smallest price:" + smallestPrice);
+// Q3
+    double radius = 7.25;
+    double area = Math.PI * Math.pow(radius, 2);
+    System.out.println("The area of the circle:" + area);
+
+//Q4
+    double myVariable = 5.0;
+    double sqRoot = Math.sqrt(myVariable);
+    System.out.println("The square root after it is set to 5 is:"  + sqRoot);
+
+// Q5
+    int x1 = 5;
+    int x2 = 85;
+    int y1 = 10;
+    int y2 = 50;
+    double distance = Math.sqrt (Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    System.out.println("Distance between points is: " + distance);
+
+//Q6
+    int variable = (int) -3.8;
+    int absoluteValue;
+    if (variable < 0) {
+        absoluteValue = -variable;
+    } else {
+        absoluteValue = variable;
+    }
+    System.out.println("Absolute value: " + absoluteValue);
+
+//Q7
+    double randomNum = Math.random();
+    System.out.println("random number between 0 and 1: " + randomNum);
+
+
+
+
+
+
+
+
+
+
+    }
+}
