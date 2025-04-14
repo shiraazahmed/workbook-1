0d6e5b6af539d3678b9827d0ef9fe974fa7ffe92
Shiraaz Ahmed <shiraaza08@gmail.com>
2025-04-14 14:13:26 -0400

    Added Loaded option for the sandwich shop

--- MathApp/src/main/java/SandwichShop.java
+++ MathApp/src/main/java/SandwichShop.java
@@ -5,11 +5,21 @@ public class SandwichShop {
         Scanner scanner = new Scanner(System.in);
         double regPrice = 5.45;
         double lgPrice = 8.65;
+        double LoadedrgPrice = 1.00;
+        double LoadedlgPrice = 1.75;
 
         System.out.println("select your size, 1 for reg and 2 for lg");
         int sizeChoice = scanner.nextInt();
 
         double basePrice = (sizeChoice == 1) ? regPrice : lgPrice;
+
+        System.out.println("Would you like your sandwich loaded (yes/no)? ");
+        String loadedChoice = scanner.next();
+
+        if (loadedChoice.equalsIgnoreCase("yes")) {
+            basePrice += (sizeChoice == 1) ? LoadedrgPrice : LoadedlgPrice;
+        }
+
 
         Scanner keyboard = new Scanner(System.in);
         System.out.println("How old are you?");
